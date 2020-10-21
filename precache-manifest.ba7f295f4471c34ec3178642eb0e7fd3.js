5db22fef87ccc97c2b7ab2085786fe968ea720e9
June Kim <june6723@naver.com>
2020-10-21 10:58:48 +0900

    Updates

---
+++ precache-manifest.ba7f295f4471c34ec3178642eb0e7fd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f0af96d353fe5bcaf1132b3d1415d8",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d46beb0696a6b1e82038",
+    "url": "/movie_app/static/css/main.664af820.chunk.css"
+  },
+  {
+    "revision": "64393d0960c07984b4a9",
+    "url": "/movie_app/static/js/2.ae8c4aa0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie_app/static/js/2.ae8c4aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46beb0696a6b1e82038",
+    "url": "/movie_app/static/js/main.fb1336f4.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
